45ea4d198329ec4d6be0f33f3cdc16fabe7704e5
Christopher Kumm <ChrisLKumm@protonmail.com>
2022-02-04 11:52:07 +0300

    Fixed `Debugger` width calculation
    Now the component renders correctly when the canvas is less than 100vw

--- src/components/Debugger/Debugger.tsx
+++ src/components/Debugger/Debugger.tsx
@@ -4,31 +4,22 @@ import { useThree } from '@react-three/fiber'
 export const Debugger = () => {
   const { viewport, size } = useThree()
 
-  const heightFraction = size.height / window.innerHeight / 3
+  const heightMesh = (viewport.height * size.height) / window.innerHeight / 3
+  const widthMesh = (viewport.width * size.width) / window.innerWidth / 3
 
   return (
     <>
       <mesh>
         <planeBufferGeometry
           attach='geometry'
-          args={[
-            viewport.width * 0.333,
-            viewport.height * heightFraction,
-            4,
-            4,
-          ]}
+          args={[widthMesh, heightMesh, 4, 4]}
         />
         <meshBasicMaterial attach='material' color='red' wireframe />
       </mesh>
       <mesh position={[0, 0, -1]}>
         <planeBufferGeometry
           attach='geometry'
-          args={[
-            viewport.width * 0.333,
-            viewport.height * heightFraction,
-            4,
-            4,
-          ]}
+          args={[widthMesh, heightMesh, 4, 4]}
         />
         <meshBasicMaterial attach='material' color='blue' wireframe />
       </mesh>
