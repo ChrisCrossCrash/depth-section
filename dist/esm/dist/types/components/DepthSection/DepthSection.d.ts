bd5afc0b5be7dc5f4a247d84de3aaed86de664a2
Christopher Kumm <ChrisLKumm@protonmail.com>
2022-02-05 14:09:50 +0300

    Rebuilt `/dist` for NPM

--- dist/esm/dist/types/components/DepthSection/DepthSection.d.ts
+++ dist/esm/dist/types/components/DepthSection/DepthSection.d.ts
@@ -1,6 +1,7 @@
 import React from 'react';
 export declare type DepthSectionProps = {
     children: React.ReactNode | null;
+    inView?: boolean;
     debug?: boolean;
 };
 /** A Three JS canvas with a custom GLTF background. */
