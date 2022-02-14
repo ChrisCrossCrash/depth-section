f1f1da2adb2542c026994401bf37c43e504c15fa
Christopher Kumm <ChrisLKumm@protonmail.com>
2022-02-14 13:50:59 +0300

    New helper function: `getSubcamSize`
    Also `getSubcamHeight` and `getSubcamWidth`. These functions return the
    height and width of the subcam.

---
+++ src/utils/getSubcamSize.tsx
@@ -0,0 +1,19 @@
+import type { RootState } from '@react-three/fiber'
+
+/** Return the height of the subcam. */
+export const getSubcamHeight = ({ viewport, size }: RootState) => {
+  return (viewport.height / 3) * (size.height / window.innerHeight)
+}
+
+/** Return the width of the subcam. */
+export const getSubcamWidth = (state: RootState) => {
+  return state.viewport.width / 3
+}
+
+/** Return the height and width of the subcam. */
+export const getSubcamSize = (state: RootState) => {
+  const width = getSubcamWidth(state)
+  const height = getSubcamHeight(state)
+
+  return [width, height] as [width: number, height: number]
+}
