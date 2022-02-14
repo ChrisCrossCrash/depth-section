d7d3c02f7bd5b7442bd27bd3a8f16e23130f2b51
Christopher Kumm <ChrisLKumm@protonmail.com>
2022-02-14 14:04:02 +0300

    Updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
+import { DepthSection } from './components/DepthSection/DepthSection'
 import { Fragmented } from './components/Fragmented/Fragmented'
+import { FloatySphere } from './components/FloatySphere/FloatySphere'
+import {
+  getSubcamHeight,
+  getSubcamWidth,
+  getSubcamSize,
+} from './utils/getSubcamSize'
+import {
+  getCameraAimPosX,
+  getCameraAimPosY,
+  getCameraAimPos,
+} from './utils/getCameraAimPos'
 
-export { Fragmented }
+export {
+  DepthSection,
+  // Pre-made depth sections
+  Fragmented,
+  FloatySphere,
+  // Utils
+  getSubcamHeight,
+  getSubcamWidth,
+  getSubcamSize,
+  getCameraAimPosX,
+  getCameraAimPosY,
+  getCameraAimPos,
+}
