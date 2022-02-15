6504a0859a7f0443d24555e8e458c0dd54728538
Christopher Kumm <ChrisLKumm@protonmail.com>
2022-02-15 11:39:37 +0300

    Fix Storybook addon order warning
    Storybook was complaining about the order of the addons in the config.
    Changing the order of the plugins fixed this.

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,8 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@storybook/addon-docs',
+    '@storybook/addon-essentials',
   ],
   staticDirs: ['../public'],
   framework: '@storybook/react',
