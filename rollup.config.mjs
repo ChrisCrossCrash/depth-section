f1ee77b182b833183a2f15ee7ce6ec1e0228c42d
Christopher Kumm <chrislkumm@protonmail.com>
2023-03-25 11:21:25 -0500

    Use official Terser plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser'
 import packageJson from './package.json' assert { type: 'json' }
 
 export default {
