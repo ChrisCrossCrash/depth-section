a723ecd2f407ff6a084d2586adad29fc857b281a
Christopher Kumm <chrislkumm@protonmail.com>
2023-04-08 09:58:55 -0500

    Prettier format

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,16 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-docs', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-essentials',
+  ],
   staticDirs: ['../public'],
   framework: {
     name: '@storybook/react-webpack5',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
-};
+    autodocs: true,
+  },
+}
