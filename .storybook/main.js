156625692e4af82d51986b6c21348318b226e2ed
Christopher Kumm <chrislkumm@protonmail.com>
2023-04-08 09:58:09 -0500

    Migrate to Storybook 7
    Used `npx storybook@latest upgrade`

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,12 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-docs',
-    '@storybook/addon-essentials',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-docs', '@storybook/addon-essentials'],
   staticDirs: ['../public'],
-  framework: '@storybook/react',
-}
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
